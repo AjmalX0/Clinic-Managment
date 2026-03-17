f66d4e9da147c41b37c13d7904e5df734b24b94f
OmegaFtp | Aditya <98910959+akaadityah@users.noreply.github.com>
2026-03-17 22:18:16 +0530

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,18 +1,26 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCSgusS1-uCtJ4uEkgm512w9QT9j0XTKTo",
-  authDomain: "nothing-c0444.firebaseapp.com",
-  projectId: "nothing-c0444",
-  storageBucket: "nothing-c0444.firebasestorage.app",
-  messagingSenderId: "956111333256",
-  appId: "1:956111333256:web:35583f2a212c312e103c3d",
-  measurementId: "G-S6K3Y6KBWM"
+  apiKey: "AIzaSyDzLBwQCpVFsPsbwyoTb5DxAXRerH5hAoQ",
+  authDomain: "clinic-management-system-1.firebaseapp.com",
+  projectId: "clinic-management-system-1",
+  storageBucket: "clinic-management-system-1.firebasestorage.app",
+  messagingSenderId: "335434952299",
+  appId: "1:335434952299:web:8820ca9432363bb25d0b2e",
+  measurementId: "G-65PLGB14LN"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
+
+export default app;
